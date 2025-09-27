c9fbba11e7974e420c9d95d52a721628a6de3e46
musarratswapnil <shaeermusarratswapnil@gmail.com>
2025-09-27 17:40:33 +0600

    gamma corrected with lab equalization

--- clock.py
+++ clock.py
@@ -9,60 +9,81 @@ def load_image(image_path):
         return None
     return image
 
-def Manual_equalizeHist(image):
-    M,N = image.shape
-    total_pixels = M * N
-    L = 256
-    histogram = cv2.calcHist([image], [0], None, [256], [0, 256]).flatten()
+def gamma_correction(image, gamma):
+    inv_gamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in range(256)]).astype(np.uint8)
+    return cv2.LUT(image, table)
+
+# def Manual_equalizeHist(image):
+#     M,N = image.shape
+#     total_pixels = M * N
+#     L = 256
+#     histogram = cv2.calcHist([image], [0], None, [256], [0, 256]).flatten()
     
-    probability = np.zeros(L, dtype=np.float32)
-    for i in range(L):
-        probability[i] = histogram[i] / total_pixels
+#     probability = np.zeros(L, dtype=np.float32)
+#     for i in range(L):
+#         probability[i] = histogram[i] / total_pixels
         
-    cum_sum_array = np.zeros(L, dtype=np.float32)
-    trans_func = np.zeros(L, dtype=np.float32)
-    for i in range(L):
-        cum_sum = 0.0
-        for j in range(i+1):
-            cum_sum += probability[j]
-        cum_sum_array[i] = cum_sum
-        trans_func[i] = (L-1) * cum_sum_array[i]
-        if trans_func[i] > 255:
-            trans_func[i] = 255
+#     cum_sum_array = np.zeros(L, dtype=np.float32)
+#     trans_func = np.zeros(L, dtype=np.float32)
+#     for i in range(L):
+#         cum_sum = 0.0
+#         for j in range(i+1):
+#             cum_sum += probability[j]
+#         cum_sum_array[i] = cum_sum
+#         trans_func[i] = (L-1) * cum_sum_array[i]
+#         if trans_func[i] > 255:
+#             trans_func[i] = 255
             
-    equalized_img = cv2.LUT(image, trans_func.astype(np.uint8))
-    return histogram, probability, cum_sum_array, trans_func, equalized_img
+#     equalized_img = cv2.LUT(image, trans_func.astype(np.uint8))
+#     return histogram, probability, cum_sum_array, trans_func, equalized_img
+
+def clahe_equalize(image):
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=8.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l)
+    lab_eq = cv2.merge((cl, a, b))
+    equalized_img = cv2.cvtColor(lab_eq, cv2.COLOR_LAB2BGR)
+    gray_eq = cv2.cvtColor(equalized_img, cv2.COLOR_BGR2GRAY)
+    return gray_eq
 
 def plot_histogram(img, title, pos):
-    plt.subplot(2, 2, pos)
+    plt.subplot(2, 3, pos)
     plt.hist(img.ravel(), bins=256, range=[0, 256], color='black')
     plt.title(title)
     plt.xlabel('Pixel value')
     plt.ylabel('Frequency')
 
-image_path = 'input images/input2.png'
+image_path = 'input images/input3.png'
 image = load_image(image_path)
 if image is None:
     exit()
 
-# Convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-hist, prob, cdf, trans, eq_img = Manual_equalizeHist(gray)
+# hist, prob, cdf, trans, eq_img = Manual_equalizeHist(gray)
+
+equalized_img = clahe_equalize(image)
 
 plt.figure(figsize=(12, 8))
-plt.subplot(2, 2, 1)
-plt.imshow(gray, cmap='gray')
+plt.subplot(2, 3, 1)
+plt.imshow(image, cmap='gray')
 plt.title('Original Image')
 plt.axis('off')
 
-plt.subplot(2, 2, 2)
-plt.imshow(eq_img, cmap='gray')
+plt.subplot(2, 3, 2)
+plt.imshow(gray, cmap='gray')
+plt.title('Gray Image')
+plt.axis('off')
+
+plt.subplot(2, 3, 3)
+plt.imshow(equalized_img, cmap='gray')
 plt.title('Equalized Image')
 plt.axis('off')
 
-plot_histogram(gray, 'Original Histogram', 3)
-plot_histogram(eq_img, 'Equalized Histogram', 4)
+plot_histogram(gray, 'Original Histogram', 4)
+plot_histogram(equalized_img, 'Equalized Histogram', 5)
 
 plt.tight_layout()
 plt.show()
